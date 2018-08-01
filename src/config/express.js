9dce1e18d98e64f7c9691f1382e687b6c190eb15
Marvin Barahona <marosvramirez@gmail.com>
2018-07-31 21:35:02 -0600

    Adding dotenv

--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,16 @@
 var express = require("express");
-// body-parser is a dependency of express. 
+// body-parser is a dependency of express.
 var bodyParser = require("body-parser");
 
 const app = express();
-var port = 4000;
+
+// Using .env
+var port = process.env.PORT || 4000;
 
 // Parse json request into req.body
 app.use(bodyParser.json());
 
+// Start app. 
 app.listen(port, () => {
   console.log("server started in port", port);
 });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+// Configure app to use .env file to set enviroment variables into process.env
+require("dotenv").config();
+
+// Load express configuration.
 var app = require("./config/express");
