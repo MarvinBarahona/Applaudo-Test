fe948d5d682e49e549c3695d0c60634120f2ac1c
Marvin Barahona <marosvramirez@gmail.com>
2018-07-31 22:22:34 -0600

    Adding mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Configure app to use .env file to set enviroment variables into process.env
 require("dotenv").config();
 
+// Load database configuration.
+var database = require("./config/database");
+
 // Load express configuration.
 var app = require("./config/express");
