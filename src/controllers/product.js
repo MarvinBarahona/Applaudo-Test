f9169e6e040959fe1aea1acaf3be60ee884e394d
Marvin Barahona <marosvramirez@gmail.com>
2018-08-07 00:07:25 -0600

    Adding Update Product route

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -49,7 +49,8 @@ function create(req, res, next){
           price: _product.price,
           stock: _product.stock
         },
-        delete: '/api/v1/products/' + _product._id
+        delete: '/api/v1/products/' + _product._id,
+        patch: '/api/v1/products/' + _product._id
       });
 
     // Any error is a server error.
@@ -94,5 +95,64 @@ function remove(req, res, next){
 }
 
 
+// Route: PATCH /api/v1/products/:productId
+// Create a product
+function update(req, res, next){
+  var id = req.params.productId;
+
+  // Body validation
+  var price = req.body.price;
+  var stock = req.body.stock;
+
+  var errors = [];
+
+  if(!price) errors.push("missing 'price' parameter");
+  else if(!Number.isInteger(price) || price < 1) errors.push("'price' must be a positive integer");
+
+  if(!stock) errors.push("missing 'stock' parameter");
+  else if(!Number.isInteger(stock) || stock < 1) errors.push("'stock' must be a positive integer");
+
+  if(errors.length > 0) next({status: 400, errors: errors});
+  // End body validation.
+
+  else{
+    // Find the product.
+    Product.findById(id).then(function(product){
+      // If not found.
+      if(!product) return Promise.reject({name: "NotFound"});
+
+      // If already deleted.
+      else if(product.active == false) return Promise.reject({name: "AlreadyDeleted"});
+
+      // Set values.
+      else{
+        product.price = price;
+        product.stock = stock;
+        return product.save();
+      }
+    }).then(function(_product){
+
+      // Success response.
+      res.status(201).json({
+        request: req.object,
+        entity: {
+          id: _product._id,
+          name: _product.name,
+          description: _product.description,
+          price: _product.price,
+          stock: _product.stock
+        },
+        delete: '/api/v1/products/' + _product._id
+      });
+
+    // Error responses.
+    }).catch(function(error){
+      if(error.name == "NotFound" || error.name == "CastError" || error.name == 'AlreadyDeleted')
+        next({status: 404, errors: ["product not found"]});
+      else next(error);
+    });
+  }
+}
+
 // module exports.
-module.exports = {create: create, remove: remove};
+module.exports = {create: create, remove: remove, update: update};
